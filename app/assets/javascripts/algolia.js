d0c34b1191c80e427eef8cdcf8177a1d35d97c1d
Matthieu Dumont <jerska@live.fr>
2017-05-05 18:04:21 +0200

    feat(search): final version

--- app/assets/javascripts/algolia.js
+++ app/assets/javascripts/algolia.js
@@ -9,62 +9,162 @@ $(document).ready(function() {
     throw new Error(message);
   }
 
-  var search = instantsearch({
+  /* global instantsearch */
+
+  app({
     appId: appId,
-    apiKey: apiKey,
+    apiKey: apiKey, // search API key
     indexName: 'Wine',
-    urlSync: true
   });
 
-  search.addWidget(
-    instantsearch.widgets.searchBox({
-      container: '#search-input',
-      placeholder: 'Search for wines...'
-    })
-  );
+  function app(opts) {
+    // ---------------------
+    //
+    //  Init
+    //
+    // ---------------------
+    const search = instantsearch({
+      appId: opts.appId,
+      apiKey: opts.apiKey,
+      indexName: opts.indexName,
+      urlSync: true,
+    });
 
-  search.addWidget(
-    instantsearch.widgets.stats({
-      container: '#stats'
-    })
-  );
+    // ---------------------
+    //
+    //  Default widgets
+    //
+    // ---------------------
+    search.addWidget(
+      instantsearch.widgets.searchBox({
+        container: '#search-input',
+        placeholder: 'Search for wine 🍷',
+      })
+    );
 
-  search.addWidget(
-    instantsearch.widgets.refinementList({
-      container: '#domain',
-      attributeName: 'domain',
-      searchForFacetValues: {
-        placeholder: 'Search for domains'
-      },
-      templates: {
-        header: '<h3>Domain</h3>',
-      }
-    })
-  );
+    search.addWidget(
+      instantsearch.widgets.hits({
+        container: '#hits',
+        hitsPerPage: 10,
+        templates: {
+          item: getTemplate('hit'),
+          empty: getTemplate('no-results'),
+        },
+        transformData: {
+          item: function(item) {
+            item.starsLayout = getStarsHTML(item.rating);
+            item.categories = getCategoryBreadcrumb(item);
+            return item;
+          },
+        },
+      })
+    );
 
-  search.addWidget(
-    instantsearch.widgets.hits({
-      container: '#results',
-      templates: {
-        item: function (hit) {
-          return '' +
-            '<li>' +
-            '  ' + hit._highlightResult.name.value + ',' +
-            '  ' + hit._highlightResult.domain.value + ' - ' +
-            '  ' + hit.year +
-            '</li>';
-        }
-      },
-      transformData: {
-        item: function (hit) {
-          // We just call this function to log the data so that
-          // you can know what you can use in your item template
-          console.log(hit);
-          return hit;
-        }
-      }
-    })
-  );
+    search.addWidget(
+      instantsearch.widgets.stats({
+        container: '#stats',
+      })
+    );
 
-  search.start();
+    search.addWidget(
+      instantsearch.widgets.pagination({
+        container: '#pagination',
+        scrollTo: '#search-input',
+      })
+    );
+
+    // ---------------------
+    //
+    //  Filtering widgets
+    //
+    // ---------------------
+    search.addWidget(
+      instantsearch.widgets.refinementList({
+        container: '#domain',
+        attributeName: 'domain',
+        sortBy: ['isRefined', 'count:desc', 'name:asc'],
+        searchForFacetValues: {
+          placeholder: 'Search for domains',
+          templates: {
+            noResults: '<div class="sffv_no-results">No matching domains.</div>',
+          },
+        },
+        templates: {
+          header: getHeader('Domain'),
+        },
+      })
+    );
+
+    search.addWidget(
+      instantsearch.widgets.rangeSlider({
+        container: '#year',
+        attributeName: 'year',
+        tooltips: {
+          format: rawValue => rawValue,
+        },
+        templates: {
+          header: getHeader('Year'),
+        },
+      })
+    );
+
+    search.addWidget(
+      instantsearch.widgets.rangeSlider({
+        container: '#price',
+        attributeName: 'price',
+        tooltips: {
+          format: function(rawValue) {
+            return `$${Math.round(rawValue).toLocaleString()}`;
+          },
+        },
+        templates: {
+          header: getHeader('Price'),
+        },
+      })
+    );
+
+    search.addWidget(
+      instantsearch.widgets.menu({
+        container: '#type',
+        attributeName: 'type',
+        sortBy: ['isRefined', 'count:desc', 'name:asc'],
+        limit: 10,
+        showMore: true,
+        templates: {
+          header: getHeader('Type'),
+        },
+      })
+    );
+
+    search.start();
+  }
+
+  // ---------------------
+  //
+  //  Helper functions
+  //
+  // ---------------------
+  function getTemplate(templateName) {
+    return document.querySelector(`#${templateName}-template`).innerHTML;
+  }
+
+  function getHeader(title) {
+    return `<h5>${title}</h5>`;
+  }
+
+  function getCategoryBreadcrumb(item) {
+    const highlightValues = item._highlightResult.categories || [];
+    return highlightValues.map(category => category.value).join(' > ');
+  }
+
+  function getStarsHTML(rating, maxRating) {
+    let html = '';
+    maxRating = maxRating || 5;
+
+    for (let i = 0; i < maxRating; ++i) {
+      html += `<span class="ais-star-rating--star${i < rating ? '' : '__empty'}"></span>`;
+    }
+
+    return html;
+  }
 });

b404b8f07fdd06605cd645b9459df22de6fd6e16
Haroen Viaene <hello@haroen.me>
2017-05-05 16:37:44 +0200

    STEP 2 SOLUTION

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var db = require('sqlite');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  db
+    .all(
+      `
+      SELECT * FROM wines
+      WHERE name LIKE '%${req.query.q}%'
+      LIMIT 5
+      `
+    )
+    .then(wines => res.render('index', { wines, query: req.query.q }));
 });
 
 module.exports = router;

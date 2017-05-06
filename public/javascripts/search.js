b49a9e2a4f8517bd97ad3c9ecca0ff72f85dddd8
Haroen Viaene <hello@haroen.me>
2017-05-06 14:20:15 +0200

    STEP 4 SOLUTION

---
+++ public/javascripts/search.js
@@ -0,0 +1,71 @@
+/* global instantsearch */
+
+app({
+  appId: 'XXX',
+  apiKey: 'XXX', // search API key
+  indexName: 'wine-search',
+});
+
+function app(opts) {
+  if (opts.appId === 'XXX') {
+    console.error('You forgot to change the API key');
+    return;
+  }
+
+  // ---------------------
+  //
+  //  Init
+  //
+  // ---------------------
+  const search = instantsearch({
+    appId: opts.appId,
+    apiKey: opts.apiKey,
+    indexName: opts.indexName,
+    urlSync: true,
+  });
+
+  // ---------------------
+  //
+  //  Default widgets
+  //
+  // ---------------------
+  search.addWidget(
+    instantsearch.widgets.searchBox({
+      container: '#search-input',
+      placeholder: 'Search for wine 🍷',
+    })
+  );
+
+  search.addWidget(
+    instantsearch.widgets.hits({
+      container: '#hits',
+      hitsPerPage: 10,
+      templates: {
+        item: getTemplate('hit'),
+      },
+      transformData: {
+        item: function(item) {
+          // We just call this function to log the data so that
+          // you can know what you can use in your item template
+          console.log(item);
+          return item;
+        },
+      },
+    })
+  );
+
+  search.start();
+}
+
+// ---------------------
+//
+//  Helper functions
+//
+// ---------------------
+function getTemplate(templateName) {
+  return document.querySelector(`#${templateName}-template`).innerHTML;
+}
+
+function getHeader(title) {
+  return `<h5>${title}</h5>`;
+}
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var db = require('sqlite');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  db
-    .all(
-      `
-      SELECT * FROM wines
-      WHERE name LIKE '%${req.query.q}%'
-      LIMIT 5
-      `
-    )
-    .then(wines => res.render('index', { wines, query: req.query.q }));
+  res.render('index');
 });
 
 module.exports = router;
